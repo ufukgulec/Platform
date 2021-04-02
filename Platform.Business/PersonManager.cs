ce4f4d29023ff4c31cd773d36e1bfdba192b0946
ufukgulec <oug505@gmail.com>
2021-04-02 17:39:57 +0300

    Person Classları

---
+++ Platform.Business/PersonManager.cs
@@ -0,0 +1,21 @@
+﻿using Platform.Dal.Abstract;
+using Platform.Entities.Models;
+using Platform.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Business
+{
+    public class PersonManager : GenericManager<Person>, IPersonService
+    {
+        IPersonRepository _personRepository;
+
+        public PersonManager(IPersonRepository personRepository) : base(personRepository)
+        {
+            _personRepository = personRepository;
+        }
+    }
+}
