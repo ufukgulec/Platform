62bcde5aa2a76a8a421abc3008da1ceab1ef87ed
ufukgulec <oug505@gmail.com>
2021-04-02 15:27:19 +0300

    PlatformContext oluşturdum.

--- Platform.Dal/Concrete/EntityFramework/Context/PlatformContext.cs
+++ Platform.Dal/Concrete/EntityFramework/Context/PlatformContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Platform.Entities.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Platform.Dal.Concrete.EntityFramework.Context
 {
-    public class PlatformContext
+    public class PlatformContext : DbContext
     {
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
