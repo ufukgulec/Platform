e55ead27e8804a8231c15523df798a0090a82e0b
ufukgulec <oug505@gmail.com>
2021-04-02 15:39:55 +0300

    Data access layer

---
+++ Platform.Dal/Abstract/IGenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Dal.Abstract
+{
+    public interface IGenericRepository<T>
+    {
+        /// <summary>
+        /// T varlığını ekleme
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>T</returns>
+        T Add(T entity);
+        /// <summary>
+        /// Id'ye göre varlık getir
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>T</returns>
+        T Get(int id);
+        /// <summary>
+        /// List Count
+        /// </summary>
+        /// <returns>Veri sayısı</returns>
+        int Count();
+        /// <summary>
+        /// Tüm T listesi
+        /// </summary>
+        /// <returns>T List</returns>
+        List<T> GetAll();
+        /// <summary>
+        /// Filter List T
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns>T List</returns>
+        List<T> GetAll(Expression<Func<T, bool>> expression);
+        /// <summary>
+        /// Id'ye göre varlık silme
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Bool</returns>
+        bool Delete(int id);
+        /// <summary>
+        /// T varlığını siler
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>Bool</returns>
+        bool Delete(T entity);
+        /// <summary>
+        /// T varlığını dönderir
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>T</returns>
+        T Update(T entity);
+    }
+}
---
+++ Platform.Dal/Concrete/EntityFramework/Repository/EfGenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using Platform.Dal.Abstract;
+using Platform.Dal.Concrete.EntityFramework.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Dal.Concrete.EntityFramework.Repository
+{
+    public class EfGenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        PlatformContext _context;
+        public EfGenericRepository(PlatformContext context)
+        {
+            _context = context;
+        }
+
+        public T Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public int Count()
+        {
+            return GetAll().Count;
+        }
+
+        public bool Delete(int id)
+        {
+            return Delete(Get(id));
+        }
+
+        public bool Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public T Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking().ToList();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().AsNoTracking().Where(expression).ToList();
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().AddOrUpdate(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+    }
+}
