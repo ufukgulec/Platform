604ef26a5149d36f393c91c8813ab8892fc50939
ufukgulec <oug505@gmail.com>
2021-04-05 13:48:39 +0300

    Like sınıfları

---
+++ Platform.Business/LikeManager.cs
@@ -0,0 +1,21 @@
+﻿using Platform.Dal.Abstract;
+using Platform.Entities.Models;
+using Platform.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Business
+{
+    public class LikeManager : GenericManager<Like>, ILikeService
+    {
+        ILikeRepository _likeRepository;
+
+        public LikeManager(ILikeRepository likeRepository) : base(likeRepository)
+        {
+            _likeRepository = likeRepository;
+        }
+    }
+}
---
+++ Platform.Dal/Concrete/EntityFramework/Repository/EfLikeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Platform.Dal.Abstract;
+using Platform.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Dal.Concrete.EntityFramework.Repository
+{
+    public class EfLikeRepository : EfGenericRepository<Like>, ILikeRepository
+    {
+        public EfLikeRepository() : base()
+        {
+
+        }
+    }
+}
