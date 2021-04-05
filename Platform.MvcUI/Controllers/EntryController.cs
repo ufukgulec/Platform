abf2e8f761d9ff42f99e1a9202cf5c3f8bc33059
ufukgulec <oug505@gmail.com>
2021-04-05 23:15:15 +0300

    Entry-Mvc Katmanında

---
+++ Platform.MvcUI/Controllers/EntryController.cs
@@ -0,0 +1,39 @@
+﻿using Platform.Entities.Models;
+using Platform.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Platform.MvcUI.Controllers
+{
+    public class EntryController : Controller
+    {
+        IEntryService entryService;
+
+        public EntryController(IEntryService entryService)
+        {
+            this.entryService = entryService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(entryService.ActiveEntryGetAll());
+        }
+        public ActionResult Post()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Post(Entry entry)
+        {
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            entryService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
