eede4b27867cf37035c7b70b242f0baeb20943e1
ufukgulec <oug505@gmail.com>
2021-04-23 01:15:30 +0300

    23.04.2021

---
+++ Platform.Entities/ViewModels/EntryView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Entities.ViewModels
+{
+    [NotMapped]
+    public class EntryView
+    {
+        public int EntryID { get; set; }
+        public string EntryTitle { get; set; }
+        public string EntryArticle { get; set; }
+        public DateTime? EntryDate { get; set; }
+
+        public bool? IsValid { get; set; }
+
+        public int? PersonID { get; set; }
+
+        public int? TagID { get; set; }
+    }
+}
